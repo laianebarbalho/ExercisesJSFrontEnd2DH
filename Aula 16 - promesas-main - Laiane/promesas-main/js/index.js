3a00d07cf1ac58b0c77e1ee1cc5ca075130807cf
Laiane Barbalho <laianebarbalho@gmail.com>
2022-06-23 21:29:12 -0300

    comitando mesa solitaria aula 16

--- Aula 16 - promesas-main - Laiane/promesas-main/js/index.js
+++ Aula 16 - promesas-main - Laiane/promesas-main/js/index.js
@@ -1,99 +1,116 @@
-const baseDeDatos = {
-    "results": [{
-        "gender": "male",
-        "name": {
-            "title": "mr",
-            "first": "brad",
-            "last": "gibson"
+const baseDeDados = {
+    "resultado": [{
+        "genero": "masculino",
+        "nome": {
+            "titulo": "sr",
+            "primeiro": "David",
+            "ultimo": "Fernando"
         },
-        "location": {
-            "street": "9278 new road",
-            "city": "kilcoole",
-            "state": "waterford",
-            "postcode": "93027",
-            "coordinates": {
-                "latitude": "20.9267",
-                "longitude": "-7.9310"
+        "localizacao": {
+            "rua": " Augusta , 238",
+            "cidade": "São Paulo",
+            "Estado": "São Paulo",
+            "postcode": "01305000",
+            "coordenadas": {
+                "latitude": "-23.5569581",
+                "longitude": "-46.6589677"
             },
             "timezone": {
-                "offset": "-3:30",
-                "description": "Newfoundland"
+                "offset": "-3:00",
+                "descricao": "Brasil"
             }
         },
-        "email": "brad.gibson@example.com",
+        "email": "david.fernando@exemplo.com",
         "login": {
             "uuid": "155e77ee-ba6d-486f-95ce-0e0c0fb4b919",
-            "username": "silverswan131",
-            "password": "firewall",
+            "usuario": "silverswan131",
+            "senha": "firewall",
             "salt": "TQA1Gz7x",
             "md5": "dc523cb313b63dfe5be2140b0c05b3bc",
             "sha1": "7a4aa07d1bedcc6bcf4b7f8856643492c191540d",
             "sha256": "74364e96174afa7d17ee52dd2c9c7a4651fe1254f471a78bda0190135dcd3480"
         },
         "dob": {
-            "date": "1993-07-20T09:44:18.674Z",
-            "age": 26
+            "data": "1993-07-20T09:44:18.674Z",
+            "idade": 26
         },
-        "registered": {
-            "date": "2002-05-21T10:59:49.966Z",
-            "age": 17
+        "registro": {
+            "data": "2002-05-21T10:59:49.966Z",
+            "idade": 17
         },
-        "phone": "011-962-7516",
-        "cell": "081-454-0666",
+        "fone": "011-4039-8745",
+        "celular": "011-99874-5621",
         "id": {
-            "name": "PPS",
-            "value": "0390511T"
+            "nome": "PPS",
+            "valor": "0390511T"
         },
-        "picture": {
-            "large": "https://randomuser.me/api/portraits/men/75.jpg",
-            "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
-            "thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+        "imagem": {
+            "grande": "https://randomuser.me/api/portraits/men/75.jpg",
+            "media": "https://randomuser.me/api/portraits/med/men/75.jpg",
+            "miniatura": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
         },
         "nat": "IE"
     }],
     "info": {
         "seed": "fea8be3e64777240",
-        "results": 1,
-        "page": 1,
-        "version": "1.3"
+        "resultado": 1,
+        "pagina": 1,
+        "versao": "1.3"
     }
 };
 
-let consultandoBaseDeDatos = new Promise((resolve, reject) => {
-    // Aquí tenemos una simulación de pedido a una base de datos, con una demora de 2 segundos.
-    // La lógica interna en realidad estará en el servidor y nosotros solo esperariamos una respuesta.
+let consultandoBaseDeDados = new Promise((resolve, reject) => {
+    // Aqui temos uma solicitação simulada para um banco de dados, com um atraso de 2 segundos.
+    //A lógica interna estará  no servidor e nós apenas esperaríamos por uma resposta.
     setTimeout(function () {
-        if (baseDeDatos == null) {
+        if (baseDeDados == null) {
             reject({
-                "mensaje": "Base de datos inexistente."
+                "mensagem": "Base de dados inexistente."
             });
         } else {
-            resolve(baseDeDatos);
+            resolve(baseDeDados);
         }
-    }, 2000);
+    }, 1000);
 
 });
 
-// Aquí realizamos un la consulta de la promesa, esperando su respuesta asíncrona
-consultandoBaseDeDatos
-    .then((respuesta) => {
-        console.log(respuesta);
-
-    }).then(
-
-    ).catch((err) => {
-        console.log(err);
+// Aqui realizamos uma consulta da promessa, aguardando sua resposta assíncrona
+consultandoBaseDeDados
+    .then((resposta) => {
+        // Aqui temos a resposta da promessa, podemos tratar a resposta como quisermos
+        renderizarDadosUsuario(resposta.resultado[0]);
+    })
+    .catch((erro) => {
+        console.log(erro);
     });
 
-function renderizarDatosUsuario(datos) {
-    /* -------------------------------- CONSIGNA -------------------------------- */
-    // Aquí deben desarrollar una función que muestre en pantalla:
-    // la foto, el nombre completo del usuario y su email.
-    // Esto debe estar basado en la info que nos llega e insertarse en el HTML.
-    // Tip: pueden manipular el CSS y estructurar la tarjeta a su gusto.
+function renderizarDadosUsuario(dados) {
+    /* -------------------------------- TAREFAS -------------------------------- */
+    // Aqui  devem desenvolver uma função que é exibida na tela:
+    // a foto, o nome completo do usuário e seu e-mail.
+    //  Isso deve ser baseado nas informações que chegam até nós e  são inseridas no HTML.
+    //  Dica: você pode manipular o CSS e estruturar o card ao seu gosto.
 
+    let quadrado = document.querySelector('.tarjeta')
+    let picture = document.createElement("img")
+    let nome = document.createElement("h1")
+    let email = document.createElement("p")
+    quadrado.style.display = 'block'
+    picture.src = dados.imagem.grande
+    picture.className = "picture"
+    picture.style.borderRadius = '20px'
+    nome.innerHTML = `${dados.nome.primeiro} ${dados.nome.ultimo}`
+    nome.style.display = 'inline'
+    nome.style.marginLeft = '15px'
 
-
-
+    email.innerHTML = dados.email
+    email.style.marginLeft = '80px'
+    email.style.textAlign = 'center'
+    document.body.appendChild(picture)
+    document.body.appendChild(email)
+    document.body.appendChild(nome)
+    quadrado.appendChild(picture)
+    quadrado.appendChild(nome)
+    quadrado.appendChild(email)
 
 }
